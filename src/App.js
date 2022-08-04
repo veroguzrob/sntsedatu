52f03ec964fb3e0f2c4719d3a17b9a6d1458f457
veroguzrob72 <vgrdesd@gmail.com>
2022-08-04 15:13:59 -0500

    Iniciando web

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>Sindicato Nacional de Trabajadores de la Secretaría de Desarrollo Agrario, Territorial y Urbano</div>
+  );
+}
+
+export default App;
