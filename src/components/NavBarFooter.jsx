a711e4dc280a20128982a4066d842861232c19ec
veroguzrob72 <vgrdesd@gmail.com>
2022-09-03 23:28:46 -0500

    Adding data to contact view

--- src/components/NavBarFooter.jsx
+++ src/components/NavBarFooter.jsx
@@ -7,15 +7,15 @@ const NavBarFooter = () => {
         <p className='title-liks'>Enlaces</p>
         <div className="container-links">
           <div className='links1' >
-            <a href="http://www.gob.mx/sedatu">SEDATU</a>
-            <a href="https://www.gob.mx/issste">ISSSTE</a>
-            <a href="https://www.gob.mx/fovissste">FOVISSSTE</a>
-            <a href="https://www.gob.mx/pensionissste">PENSIONISSSTE</a>
-            <a href="https://oficinavirtual.issste.gob.mx/">SINAVID</a>
+            <a className='links-footer' href="http://www.gob.mx/sedatu">SEDATU</a>
+            <a className='links-footer' href="https://www.gob.mx/issste">ISSSTE</a>
+            <a className='links-footer' href="https://www.gob.mx/fovissste">FOVISSSTE</a>
+            <a className='links-footer' href="https://www.gob.mx/pensionissste">PENSIONISSSTE</a>
+            <a className='links-footer' href="https://oficinavirtual.issste.gob.mx/">SINAVID</a>
           </div>
           <div className='links2' >
-            <a href="https://sistemas.sedatu.gob.mx/e-recibos/#">E-RECIBOS</a>
-            <a href="https://mail.sedatu.gob.mx/">ZIMBRA</a>
+            <a className='links-footer' href="https://sistemas.sedatu.gob.mx/e-recibos/#">E-RECIBOS</a>
+            <a className='links-footer' href="https://mail.sedatu.gob.mx/">ZIMBRA</a>
           </div>
         </div>
       </div>
